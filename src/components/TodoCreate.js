14be549591787d712a73f584660cd52f653b0e33
hyeran03 <user@email.mail>
2019-12-17 17:43:46 +0900

    TodoList - 프로젝트 생성 UI 준비3 - TodoCreate(CircleButton 클릭시 input이 나옴)

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
 
-const CirclButtom = styled.button`
+const CircleButtom = styled.button`
   background: #38d9a9;
   &:hover {
     background: #63e6be;
@@ -47,14 +47,49 @@ const CirclButtom = styled.button`
     `}
 `;
 
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
+
 function TodoCreate() {
   const [open, setOpen] = useState(false);
   const onToggle = () => setOpen(!open);
 
   return (
-    <CirclButtom onClick={onToggle} open={open}>
-      <MdAdd />
-    </CirclButtom>
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>
+            <Input placeholder="할 일을 입력 후, Enter 를 누르세요" autoFocus />
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButtom onClick={onToggle} open={open}>
+        <MdAdd />
+      </CircleButtom>
+    </>
   );
 }
 
