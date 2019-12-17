223e977d76ab36e37b4449ab0261fa4257fd35ef
hyeran03 <user@email.mail>
2019-12-17 22:11:22 +0900

    기능 구현 하기

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const TodoHeadBlock = styled.div`
   /* 내부에 특정을 넣어 바로 선택하도록 */
@@ -26,11 +27,26 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  //할일 개수 변화 -done: true or false에따라
+  const todos = useTodoState();
+  const undoneTasks = todos.filter(todo => !todo.done);
+
+  //날짜 변화(javsscript)
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+  const dayName = today.toLocaleDateString("ko-KR", {
+    weekday: "long"
+  });
+
   return (
     <TodoHeadBlock>
-      <h1>2019년 12월 17일</h1>
-      <div className="day">화요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
+import { useTodoDispatch } from "../TodoContext";
 
 //좌측에 체크박스를 보여주는 컴포넌트
 const CheckCircle = styled.div`
@@ -56,7 +57,7 @@ const TodoItemBlcok = styled.div`
   padding-top: 12px;
   padding-bottom: 12px;
 
- /* Remove를 어떤 특정사항에만 넣어주고 싶다 - seleter사용*/
+  /* Remove를 어떤 특정사항에만 넣어주고 싶다 - seleter사용*/
   &:hover {
     ${Remove} {
       opacity: 1;
@@ -65,15 +66,29 @@ const TodoItemBlcok = styled.div`
 `;
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const onToggle = () =>
+    dispatch({
+      type: "TOGGLE",
+      id
+    });
+  const onRemove = () =>
+    dispatch({
+      type: "REMOVE",
+      id
+    });
+
   return (
     <TodoItemBlcok>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
     </TodoItemBlcok>
   );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
