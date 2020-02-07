743b89d62f1a5a993d7694ea94ae0af5dce4d370
hyeran03 <user@email.mail>
2020-02-07 13:31:25 +0900

    수정중

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ import TodoItem from "./TodoItem";
 import { useTodoState } from "../TodoContext";
 
 const TodoListBlock = styled.div`
-  flex: 1; /* flex가 사용된 사황에서 1은 자신이 차지할수 있는부분 다차지*/
+  flex: 1;
   padding-bottom: 48xp;
   padding: 20px 32px;
   overflow-y: auto;
