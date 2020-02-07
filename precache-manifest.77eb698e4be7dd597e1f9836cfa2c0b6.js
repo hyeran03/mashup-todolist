2972a18d09052f45f083cb935b85cff3649a724e
hyeran03 <user@email.mail>
2020-02-07 14:53:35 +0900

    Updates

---
+++ precache-manifest.77eb698e4be7dd597e1f9836cfa2c0b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a022b2a7eb7ddd213627df97d5aad2",
+    "url": "/mashup_todolist/index.html"
+  },
+  {
+    "revision": "4489af52a794006adcba",
+    "url": "/mashup_todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "369fc06c272938095ec7",
+    "url": "/mashup_todolist/static/js/2.d68f3fb6.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/mashup_todolist/static/js/2.d68f3fb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4489af52a794006adcba",
+    "url": "/mashup_todolist/static/js/main.589d2ead.chunk.js"
+  },
+  {
+    "revision": "e80b75892749cd671127",
+    "url": "/mashup_todolist/static/js/runtime-main.07085730.js"
+  }
+]);
